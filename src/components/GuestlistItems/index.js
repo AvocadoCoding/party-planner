eee5e880e94cada0f2c2dd9c0bb9781c9837fb8b
francescasnatt <116296472+francescasnatt@users.noreply.github.com>
2023-02-22 21:36:30 +0000

    added functionality to guestlist buttons

--- src/components/GuestlistItems/index.js
+++ src/components/GuestlistItems/index.js
@@ -1,20 +1,38 @@
 import React from "react";
 
 const GuestlistItems = ({guests, setGuests}) => {
-    console.log(guests)
+    
+    const handleComplete = (guest) => {
+        setGuests(
+            guests.map((item) =>{
+                if(item.id === guest.id) {
+                    return {...item, completed: !item.completed}
+                }
+                return item;
+            })
+            );
+    }
+
+    const handleDelete = ({id}) => {
+        setGuests(guests.filter((guest) => guest.id !== id));
+    }
+
+
+
+
     return (
         <ul>
             {guests.map((guest) => (
                 <li className="guest-item" key={guest.id}>
                     {guest.title}
                     <div>
-                        <button className="button-complete">
+                        <button className="button-complete" onClick={() => handleComplete(guest)}>
                         ✅
                         </button>
                         <button className="button-edit">
                         ✏️
                         </button>
-                        <button className="button-delete">
+                        <button className="button-delete" onClick={() => handleDelete(guest)}>
                         🗑️
                         </button>
                     </div>
