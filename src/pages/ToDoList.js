dc9aefbadd6779ee0ea7aa942c4b7fc816d2441f
Ben Jervis <117092819+BenjaminJervis@users.noreply.github.com>
2023-02-22 20:51:21 +0000

    Updated ToDoJS form
    Added code to ToDoJS

--- src/pages/ToDoList.js
+++ src/pages/ToDoList.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
 
-function ToDo() {
-  return (
-    <div>
-
-    </div>
-  );
+    this.state = {
+      items: []
+    };
+ 
+    this.addItem = this.addItem.bind(this);
+  }
+   
+  addItem(e) {
+    if (this._inputElement.value !== "") {
+      var newItem = {
+        text: this._inputElement.value,
+        key: Date.now()
+      };
+   
+      this.setState((prevState) => {
+        return { 
+          items: prevState.items.concat(newItem) 
+        };
+      });
+     
+      this._inputElement.value = "";
+    }
+     
+    console.log(this.state.items);
+       
+    e.preventDefault();
+  }
+ 
+  
+  render() {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input ref={(a) => this._inputElement = a} 
+                    placeholder="please enter party task">
+            </input>
+            <button type="submit">add</button>
+          </form>
+        </div>
+        <TodoItems entries={this.state.items}/>
+      </div>
+    );
+  }
 }
 
-export default ToDo;
+export default TodoList;
+ 
