6fa64842c267faea6a3db1795ddf59dfd69b7c65
Ben Jervis <117092819+BenjaminJervis@users.noreply.github.com>
2023-02-24 20:03:18 +0000

    Update code from class to functional

---
+++ src/components/ToDoFunction/index.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+
+function ToDoFunction() {
+    const [tasksRemaining, setTasksRemaining] = useState(0);
+    const [task, setTask]=useState()
+    const [tasks, setTasks] = useState([
+        {
+            title: "Task name",
+            completed: true
+        },
+        {
+            title: "Task description",
+            completed: true
+        },
+        {
+            title: "Task due date",
+            completed: false
+        }
+    ]);
+      
+    useEffect(() => { 
+      setTasksRemaining(tasks.filter(task => !task.completed).length) 
+    });
+
+    const addTask = title => {
+        const newTasks = [...tasks, { title, completed: false }];
+        setTasks(newTasks);
+    };
+    
+    const completeTask = index => {
+        const newTasks = [...tasks];
+        newTasks[index].completed = true;
+        setTasks(newTasks);
+    };
+    
+    const removeTask = index => {
+        const newTasks = [...tasks];
+        newTasks.splice(index, 1);
+        setTasks(newTasks);
+    };
+
+    return (
+        <div className="todo-container">
+            <div className="header">Pending tasks ({tasksRemaining})</div>
+            <div className="tasks">
+                {tasks.map((task, index) => (
+                    <Task
+                    task={task}
+                    index={index}
+                    completeTask={completeTask}
+                    removeTask={removeTask}
+                    key={index}
+                    />
+                ))}
+            </div>
+            <div className="create-task" >
+                <TaskForm task={task} addTask={addTask} setTask={setTask}/>
+            </div>
+        </div>
+    );
+}
+
+export default ToDoFunction;
--- src/pages/ToDoList.js
+++ src/pages/ToDoList.js
@@ -1,54 +1,14 @@
-import React, { Component } from "react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ToDoFunction from '../components/ToDoFunction';
 
-class TodoList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      items: []
-    };
- 
-    this.addItem = this.addItem.bind(this);
-  }
-   
-  addItem(e) {
-    if (this._inputElement.value !== "") {
-      var newItem = {
-        text: this._inputElement.value,
-        key: Date.now()
-      };
-   
-      this.setState((prevState) => {
-        return { 
-          items: prevState.items.concat(newItem) 
-        };
-      });
-     
-      this._inputElement.value = "";
-    }
-     
-    console.log(this.state.items);
-       
-    e.preventDefault();
-  }
- 
-  
-  render() {
+function ToDo() {
     return (
-      <div className="todoListMain">
-        <div className="header">
-          <form onSubmit={this.addItem}>
-            <input ref={(a) => this._inputElement = a} 
-                    placeholder="please enter party task">
-            </input>
-            <button type="submit">add</button>
-          </form>
-        </div>
-        <TodoItems entries={this.state.items}/>
+      <div>
+        <ToDoFunction/>
       </div>
     );
   }
-}
-
-export default TodoList;
+  
+  export default ToDo;
  
