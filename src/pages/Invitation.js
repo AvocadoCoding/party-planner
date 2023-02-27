b9918c175b1a8491f60baf8aa5e600c640530a68
francescasnatt <116296472+francescasnatt@users.noreply.github.com>
2023-02-27 18:13:59 +0000

    added invitation form and initial styling

--- src/pages/Invitation.js
+++ src/pages/Invitation.js
@@ -1,10 +1,11 @@
 import React from "react";
+import InvitationForm from "../components/Invitation";
 
 
 function Invitation() {
   return (
     <div>
-      
+      <InvitationForm />
     </div>
   );
 }
