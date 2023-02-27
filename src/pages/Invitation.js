6260535a6010b5535120944ce9de52a195f89848
francescasnatt <116296472+francescasnatt@users.noreply.github.com>
2023-02-27 22:07:45 +0000

    updates

--- src/pages/Invitation.js
+++ src/pages/Invitation.js
@@ -1,11 +1,23 @@
 import React from "react";
 import InvitationForm from "../components/Invitation";
+// import Pdf from "react-to-pdf";
 
 
 function Invitation() {
   return (
     <div>
       <InvitationForm />
+     
+      {/* <Pdf targetRef={ref} filename="code-example.pdf">
+        {({ toPdf }) => <button onClick={toPdf}>Generate Pdf</button>}
+      </Pdf> */}
+
+      {/* <ReactToPdf>
+        {({toPdf, targetRef}) =>  (
+          <div style={{width: 500, height: 500, background: 'red'}} onClick={toPdf} ref={targetRef}/>
+        )}
+      </ReactToPdf> */}
+
     </div>
   );
 }
