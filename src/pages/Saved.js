7c644f6fea09080bf600e1cf6dbea3c57d635991
AvocadoCoding <116954089+AvocadoCoding@users.noreply.github.com>
2023-02-27 11:13:55 +0000

    Mapping and cards added to save page

--- src/pages/Saved.js
+++ src/pages/Saved.js
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useState} from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 
 function Saved() {
-  return (
-    <div>
 
-    </div>
+// get local storage for button toogle, if nothing in local storage return
+// empty array
+const initialState = JSON.parse(localStorage.getItem("drinks")) || [];
+const [drinkStorage, setDrinkStorage] = useState(initialState);
+console.log(drinkStorage);
+
+//Function to delete item from array
+const handleDelete = ({id}) => {
+  setDrinkStorage(drinkStorage.filter((drink) => drink.id !== id));
+}
+
+
+// Map saved local storage drinks to cards in page
+  return (
+    <Row>
+    {drinkStorage.map((currentDrink)=> (
+      <Col xs={12} sm={6} lg={3} key={currentDrink.idDrink}>
+        <Card style={{ width: '18rem', margin: '10px', borderColor: 'black'}}>
+          <Card.Img variant="top" src={currentDrink.strDrinkThumb}/>
+            <Card.Body style={{backgroundColor: '#49C2BC'}}>
+              <div className='title'>
+              <Card.Title>{currentDrink.strDrink}</Card.Title>
+              </div>
+                <h3>Instructions</h3>
+                <p>{currentDrink.strInstructions}</p>
+                <h3>Ingredients</h3>
+                <ul>
+                  <li>{currentDrink.strIngredient1} {currentDrink.strMeasure1}</li>
+                  <li>{currentDrink.strIngredient2} {currentDrink.strMeasure2}</li>
+                  <li>{currentDrink.strIngredient3} {currentDrink.strMeasure3}</li>
+                  <li>{currentDrink.strIngredient4} {currentDrink.strMeasure4}</li>
+                </ul>
+                {/* Delete button to remove item */}
+                <button className="button-delete button" onClick={() => handleDelete(currentDrink)}>
+                        🗑️
+                </button>
+            </Card.Body>
+        </Card>
+      </Col>
+      ))}
+    </Row>
+
+    
   );
 }
 
